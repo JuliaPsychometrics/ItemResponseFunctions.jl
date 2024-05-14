41f6c59ef99abb878c3a2eab85c2c0e8736662f4
p-gw <p.gewessler@gmx.at>
2024-05-14 10:53:53 +0200

    pkg formatting

--- src/ItemResponseFunctions.jl
+++ src/ItemResponseFunctions.jl
@@ -9,51 +9,17 @@ using SimpleUnPack
 import AbstractItemResponseModels: response_type, Dichotomous
 
 using LogExpFunctions: logistic
+using DocStringExtensions: SIGNATURES, TYPEDEF, METHODLIST
 
 export OneParameterLogisticModel, OnePL
 export TwoParameterLogisticModel, TwoPL
 export ThreeParameterLogisticModel, ThreePL
 export FourParameterLogisticModel, FourPL
 
-abstract type DichotomousItemResponseModel <: ItemResponseModel end
-
-response_type(::Type{<:DichotomousItemResponseModel}) = Dichotomous
-
-include("models/dichotomous/4PL.jl")
-include("models/dichotomous/3PL.jl")
-include("models/dichotomous/2PL.jl")
-include("models/dichotomous/1PL.jl")
-
-"""
-    expected_score
-"""
-function expected_score(
-    M::Type{<:DichotomousItemResponseModel},
-    theta::T,
-    betas::AbstractVector;
-    scoring_function::F = identity,
-) where {T<:Real,F}
-    score = zero(T)
-
-    for beta in betas, y in 0:1
-        score += irf(M, theta, beta, y) * scoring_function(y)
-    end
-
-    return score
-end
-
-function information(
-    M::Type{<:DichotomousItemResponseModel},
-    theta::T,
-    betas::AbstractVector,
-) where {T<:Real}
-    info = zero(T)
-
-    for beta in betas
-        info += iif(M, theta, beta, 1)
-    end
-
-    return info
-end
+include("model_types.jl")
+include("irf.jl")
+include("iif.jl")
+include("expected_score.jl")
+include("information.jl")
 
 end
