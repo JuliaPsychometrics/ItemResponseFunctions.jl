122f2186535eb3147fc9ceaaa8e8110032786581
p-gw <p.gewessler@gmx.at>
2024-05-14 10:54:04 +0200

    add docs

---
+++ docs/make.jl
@@ -0,0 +1,27 @@
+using ItemResponseFunctions
+using Documenter
+
+DocMeta.setdocmeta!(
+    ItemResponseFunctions,
+    :DocTestSetup,
+    :(using ItemResponseFunctions);
+    recursive = true,
+)
+
+makedocs(;
+    checkdocs = :exported,
+    modules = [ItemResponseFunctions],
+    authors = "Philipp Gewessler",
+    repo = "https://github.com/p-gw/ItemResponseFunctions.jl/blob/{commit}{path}#{line}",
+    sitename = "ItemResponseFunctions.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://p-gw.github.io/ItemResponseFunctions.jl",
+        edit_link = "main",
+        assets = String[],
+    ),
+    pages = ["Home" => "index.md"],
+    plugins = [],
+)
+
+deploydocs(; repo = "github.com/p-gw/ItemResponseFunctions.jl", devbranch = "main")
