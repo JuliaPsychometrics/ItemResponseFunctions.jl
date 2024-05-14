1253e6a4afb128d1b36bbd5a805acffa0bf46673
Documenter.jl <documenter@juliadocs.github.io>
2024-05-14 11:05:57 +0000

    build based on 6eafcba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ItemResponseFunctions.jl","page":"Home","title":"ItemResponseFunctions.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"(Image: Stable) (Image: Dev) (Image: Build Status) (Image: Coverage)","category":"page"},{"location":"","page":"Home","title":"Home","text":"ItemResponseFunctions.jl implements basic functions for Item Response Theory models. It is built based on the interface designed in AbstractItemResponseModels.jl.","category":"page"}]
+}
