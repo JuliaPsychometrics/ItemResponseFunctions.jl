f885a43875ba641a4ef0d3f6ea248a6ffc148034
p-gw <p.gewessler@gmx.at>
2024-05-15 11:05:06 +0200

    run tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,5 @@ using ItemResponseFunctions
     include("models/two_parameter_logistic.jl")
     include("models/three_parameter_logistic.jl")
     include("models/four_parameter_logistic.jl")
+    include("models/generalized_partial_credit_model.jl")
 end
