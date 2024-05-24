462b7a9083807adb8f1e19ba9a3e0c0d82c6a653
Philipp Gewessler <p.gewessler@gmx.at>
2024-05-24 16:41:58 +0200

    doc fixes

--- docs/make.jl
+++ docs/make.jl
@@ -12,12 +12,12 @@ makedocs(;
     checkdocs = :exported,
     modules = [ItemResponseFunctions],
     authors = "Philipp Gewessler",
-    repo = "https://github.com/JuliaPsychometrics/ItemResponseFunctions.jl/blob/{commit}{path}#{line}",
     sitename = "ItemResponseFunctions.jl",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
         canonical = "https://juliapsychometrics.github.io/ItemResponseFunctions.jl",
         edit_link = "main",
+        repolink = "https://github.com/JuliaPsychometrics/ItemResponseFunctions.jl/blob/{commit}{path}#{line}",
         assets = String[],
     ),
     pages = ["Home" => "index.md", "API" => "api.md"],
