f1895414a7ba35cf439dc90652b059b3fb5b6764
Philipp Gewessler <p.gewessler@gmx.at>
2024-05-25 18:34:51 +0200

    fix doctests

--- src/iif.jl
+++ src/iif.jl
@@ -9,29 +9,36 @@ If `y` is omitted, the item category information functions for all categories ar
 ## Examples
 ### 1 Parameter Logistic Model
 ```jldoctest
-julia> iif(OnePL, 0.0, 0.0)
-0.25
+julia> iif(OnePL, 0.0, 0.0, 1)
+0.125
 
 julia> iif(OnePL, 0.0, (; b = 0.0))
-0.25
+2-element Vector{Float64}:
+ 0.125
+ 0.125
 ```
 
 ### 2 Parameter Logistic Model
 ```jldoctest
 julia> iif(TwoPL, 0.0, (a = 1.3, b = 0.2))
-0.4154394331315018
+2-element Vector{Float64}:
+ 0.2345721809921237
+ 0.1808672521393781
 ```
 
 ### 3 Parameter Logistic Model
 ```jldoctest
 julia> iif(ThreePL, 0.0, (a = 1.5, b = 0.5, c = 0.15))
-0.3162871805861734
-
+2-element Vector{Float64}:
+ 0.2830301834782102
+ 0.033256997107963204
 ```
 ### 4 Parameter Logistic Model
 ```jldoctest
-julia> iif(FourPL, 0.0, (a = 2.1, b = -1.5, c = 0.15, d = 0.9))
-0.03387157823313065
+julia> iif(FourPL, 0.0, (a = 2.1, b = -0.2, c = 0.15, d = 0.9))
+2-element Vector{Float64}:
+ 0.19363288880050672
+ 0.39951402052782453
 ```
 
 """
