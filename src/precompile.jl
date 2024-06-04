c1eed233ade717fa7a19b4146d4231b5f2b8d923
p-gw <p.gewessler@gmx.at>
2024-06-04 11:28:01 +0200

    fix doctests

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,8 +1,6 @@
 using PrecompileTools: @setup_workload, @compile_workload
 
 @setup_workload begin
-    import ForwardDiff
-
     models = [OnePL, OnePLG, TwoPL, ThreePL, FourPL, FivePL, PCM, GPCM, RSM, GRSM]
     beta = (a = 1.0, b = 0.0, c = 0.0, d = 1.0, e = 1.0, t = zeros(3))
     betas = fill(beta, 3)
