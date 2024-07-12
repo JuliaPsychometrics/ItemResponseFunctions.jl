46c1b51571682b0056e564ac42a3aa2efb548cd6
Philipp Gewessler <p.gewessler@gmx.at>
2024-07-12 14:39:27 +0200

    benchmark dichotomous models

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,11 +3,47 @@ using ItemResponseFunctions
 
 const SUITE = BenchmarkGroup()
 
-SUITE["OnePL"] = BenchmarkGroup()
+models = [OnePL, TwoPL, ThreePL, FourPL, FivePL]
 
-SUITE["OnePL"]["irf"] = @benchmarkable(
-    irf(OnePL, theta, beta, $1),
-    evals = 10,
-    samples = 1000,
-    setup = (theta = randn(); beta = (; b = randn()))
-)
+function make_pars(; nthresholds = 3)
+    a = rand() * 2
+    b = randn()
+    c = rand() * 0.5
+    d = rand() * 0.5 + 0.5
+    e = 1 + rand() * 0.5
+    t = randn(nthresholds)
+    return (; a, b, c, d, e, t)
+end
+
+for model in models
+    m = string(model)
+    SUITE[m] = BenchmarkGroup()
+
+    SUITE[m]["irf"] = @benchmarkable(
+        irf(model, theta, beta, $1),
+        evals = 10,
+        samples = 1000,
+        setup = (theta = randn(); beta = make_pars())
+    )
+
+    SUITE[m]["iif"] = @benchmarkable(
+        iif(model, theta, beta, $1),
+        evals = 10,
+        samples = 1000,
+        setup = (theta = randn(); beta = make_pars())
+    )
+
+    SUITE[m]["expected_score"] = @benchmarkable(
+        expected_score(model, theta, betas, $1),
+        evals = 10,
+        samples = 1000,
+        setup = (theta = randn(); betas = [make_pars() for _ in 1:20])
+    )
+
+    SUITE[m]["information"] = @benchmarkable(
+        information(model, theta, betas, $1),
+        evals = 10,
+        samples = 1000,
+        setup = (theta = randn(); betas = [make_pars() for _ in 1:20])
+    )
+end
